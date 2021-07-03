2fd6d50ec130d93a9937305e948c43b02c362bb7
himo <39026824+himo@users.noreply.github.com>
2021-07-04 00:40:07 +0900

    Add files via upload

--- share/index.js
+++ share/index.js
@@ -11,6 +11,7 @@ const mediaStreamConstraints = {
     video: {
       //width: 1280,
       //height: 720, 
+      mediaSource: "screen",
       "frameRate": {"max": 60}
     } 
   };
@@ -21,15 +22,15 @@ var iswebsocket=0
 var webSocket
 //スタートボタンが押されたときidboxに入ってるidでpeer connectを行う
 document.getElementById('start').onclick = function() {
-    navigator.mediaDevices
-    .getDisplayMedia(mediaStreamConstraints)
-    .then(gotLocalMediaStream)
-    .catch(handleLocalMediaStreamError);
     ID=document.getElementById("set-id").value
     if (ID!=""){
         peer = new Peer(ID,{
             key: 'c2ad39ff-ed02-41e1-b1f1-c918871c1f28',
             debug: 3
+        });
+        peer.on('error', e => {
+            document.getElementById('my-id').textContent = "指定したIDは使用することができないので別のIDに変更するかランダムなIDでの画面共有をお願いします";
+            document.getElementById('my-id2').textContent = "IDは画面共有に成功した際に表示されます";
         });
         peer.on('call', mediaConnection => {
             mediaConnection.answer(localStream, {videoBandwidth: 14000, audioBandwidth: 4000});
@@ -38,7 +39,18 @@ document.getElementById('start').onclick = function() {
         //peerが接続できたときidを画面に表示する
         peer.on('open', () => {
             console.log("connected")
-            document.getElementById('my-id').textContent = peer.id;
+            document.getElementById('my-id').textContent = "ID："+peer.id;
+            document.getElementById('my-id2').textContent = "";
+            document.getElementById('set-id').style.display ="none"
+            document.getElementById('start').style.display ="none"
+            document.getElementById('start_r').style.display ="none"
+            document.getElementById('op2').style.display ="none"
+            document.getElementById('url').style.display ="block"
+            navigator.clipboard.writeText(location.protocol + "//" + location.hostname + location.pathname.replace('share/', 'operation')+"?id="+peer.id);
+            navigator.mediaDevices
+            .getDisplayMedia(mediaStreamConstraints)
+            .then(gotLocalMediaStream)
+            .catch(handleLocalMediaStreamError);
         });
         //detachannelに接続できたとき
         peer.on("connection", (conn) => {
@@ -51,6 +63,42 @@ document.getElementById('start').onclick = function() {
             });
         });
     }
+}
+//スタートボタンが押されたときidboxに入ってるidでpeer connectを行う
+document.getElementById('start_r').onclick = function() {
+    peer = new Peer(ID,{
+        key: 'c2ad39ff-ed02-41e1-b1f1-c918871c1f28',
+        debug: 3
+    });
+    peer.on('call', mediaConnection => {
+        mediaConnection.answer(localStream, {videoBandwidth: 14000, audioBandwidth: 4000});
+        setEventListener(mediaConnection);
+    });
+    //peerが接続できたときidを画面に表示する
+    peer.on('open', () => {
+        console.log("connected")
+        document.getElementById('my-id').textContent = "ID："+peer.id;
+        document.getElementById('my-id2').textContent = "";
+        navigator.mediaDevices
+        .getDisplayMedia(mediaStreamConstraints)
+        .then(gotLocalMediaStream)
+        .catch(handleLocalMediaStreamError);
+    });
+    //detachannelに接続できたとき
+    peer.on("connection", (conn) => {
+        //document.getElementById('getconnected').textContent = "接続されました";
+        //dataが送られたとき発火
+        conn.on("data", (name) => {
+            //console.log(`${name}: ${msg}`);
+            // => 'SkyWay: Hello, World!'
+            webSocket.send(name);
+        });
+    });
+}
+
+document.getElementById('url').onclick = function() {
+    navigator.clipboard.writeText(location.protocol + "//" + location.hostname + location.pathname.replace('share/', 'operation')+"?id="+peer.id);
+    document.getElementById('copyy').textContent = "コピーされました";
 }
 
 window.onload = function(){
